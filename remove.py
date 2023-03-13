0c87cbe9a376d98d55f5c0e119fb26df71a9fe41
AdithyakrishnaV <adithyakr007@gmail.com>
2023-03-13 20:01:10 +0530

    remove items form string, url, etc..

---
+++ remove.py
@@ -0,0 +1,10 @@
+def decode():
+  string=input("Enter the encrypted string: \n")
+  num=int(input("Enter number of items to remove: "))
+  new=string
+  i=1
+  for i in range(num):
+    change=input("Enter the item to remove: ")
+    new=new.replace(change,"")
+  print(new)
+decode()
